28d55e58b7b974863ce1473d6d51d231208db7c2
Rafael G Firmino <rafaelgfirmino@gmail.com>
2016-11-22 17:51:24 -0200

    Add configuration API key

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,9 +20,10 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('fcm_http');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+            ->scalarNode('autentication_api_key')->isRequired()->cannotBeEmpty()->end()
+            ->end();
 
         return $treeBuilder;
     }
--- DependencyInjection/FcmHttpExtension.php
+++ DependencyInjection/FcmHttpExtension.php
@@ -22,6 +22,8 @@ class FcmHttpExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $container->setParameter('fcm_http.autentication_api_key', $config['autentication_api_key']);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
