340251a26fe957caf1a844e65de9f929259f4d18
Rafael G Firmino <rafaelgfirmino@gmail.com>
2016-11-22 23:34:11 -0200

    Add partial usage

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,12 +2,18 @@
 
 namespace DigitalAp\FcmHttpBundle\Controller;
 
+use DigitalAp\FcmHttpBundle\Entity\Message;
+use DigitalAp\FcmHttpBundle\Entity\Notification;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $notification = new Notification('teste','body');
+        $message = (new Message(['1','2','3','4']))->setNotification($notification);
+        dump($this->get('fcm_http.send')->send($message));
+
         return $this->render('FcmHttpBundle:Default:index.html.twig');
     }
 }
