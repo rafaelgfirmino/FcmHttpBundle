01b29e6795608a88fa5313dc1c03d242e96789d2
Rafael G Firmino <rafaelgfirmino@gmail.com>
2016-11-24 10:22:28 -0200

    Add all documentation properties

--- Entity/Message.php
+++ Entity/Message.php
@@ -1,46 +1,31 @@
 <?php
-
 namespace DigitalAp\FcmHttpBundle\Entity;
-
 
 use DigitalAp\FcmHttpBundle\Model\FCM;
 use Doctrine\Common\Collections\ArrayCollection;
 
 class Message
 {
-    private $priority;
-    private $time_to_live;
-    private $delay_while_idle;
-    private $to;
+    //Targets
     private $registration_ids;
+    private $condition;
+
+    //Options
+    private $collapse_key;
+    private $priority;
+    private $content_available;
+    private $delay_while_idle;
+    private $time_to_live;
+    private $restricted_package_name;
+    private $dry_run;
     private $notification;
     private $data;
 
     public function __construct(array $registration_ids)
     {
         $this->registration_ids = $registration_ids;
-        if (count($registration_ids) == 1) {
-            $this->to = $registration_ids[0];
-            $this->registration_ids = null;
-        }
         $this->priority = FCM::MESSAGE_PRIORITY_NORMAL;
         $this->data = new ArrayCollection();
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getTo()
-    {
-        return $this->to;
-    }
-
-    /**
-     * @param mixed $to
-     */
-    public function setTo($to)
-    {
-        $this->to = $to;
     }
 
     /**
@@ -58,6 +43,44 @@ class Message
     public function setRegistration_ids($registration_ids)
     {
         $this->registration_ids = $registration_ids;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCondition()
+    {
+        return $this->condition;
+    }
+
+    /**
+     * @param mixed $condition
+     * @return Message
+     */
+    public function setCondition($condition)
+    {
+        $this->condition = $condition;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCollapse_key()
+    {
+        return $this->collapse_key;
+    }
+
+    /**
+     * @param mixed $collapse_key
+     * @return Message
+     */
+    public function setCollapse_key($collapse_key)
+    {
+        $this->collapse_key = $collapse_key;
 
         return $this;
     }
@@ -82,7 +105,45 @@ class Message
     }
 
     /**
-     * @return null
+     * @return mixed
+     */
+    public function getContent_available()
+    {
+        return $this->content_available;
+    }
+
+    /**
+     * @param mixed $content_available
+     * @return Message
+     */
+    public function setContent_available($content_available)
+    {
+        $this->content_available = $content_available;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDelay_while_idle()
+    {
+        return $this->delay_while_idle;
+    }
+
+    /**
+     * @param mixed $delay_while_idle
+     * @return Message
+     */
+    public function setDelay_while_idle($delay_while_idle)
+    {
+        $this->delay_while_idle = $delay_while_idle;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
      */
     public function getTime_to_live()
     {
@@ -90,12 +151,50 @@ class Message
     }
 
     /**
-     * @param null $time_to_live
+     * @param mixed $time_to_live
      * @return Message
      */
     public function setTime_to_live($time_to_live)
     {
         $this->time_to_live = $time_to_live;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRestricted_package_name()
+    {
+        return $this->restricted_package_name;
+    }
+
+    /**
+     * @param mixed $restricted_package_name
+     * @return Message
+     */
+    public function setRestricted_package_name($restricted_package_name)
+    {
+        $this->restricted_package_name = $restricted_package_name;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDry_run()
+    {
+        return $this->dry_run;
+    }
+
+    /**
+     * @param mixed $dry_run
+     * @return Message
+     */
+    public function setDry_run($dry_run)
+    {
+        $this->dry_run = $dry_run;
 
         return $this;
     }
@@ -120,7 +219,7 @@ class Message
     }
 
     /**
-     * @return array
+     * @return ArrayCollection
      */
     public function getData()
     {
@@ -128,7 +227,7 @@ class Message
     }
 
     /**
-     * @param array $data
+     * @param ArrayCollection $data
      * @return Message
      */
     public function setData($data)
@@ -137,21 +236,4 @@ class Message
 
         return $this;
     }
-
-    /**
-     * @return mixed
-     */
-    public function getDelay_while_idle()
-    {
-        return $this->delay_while_idle;
-    }
-
-    /**
-     * @param mixed $delay_while_idle
-     */
-    public function setDelay_while_idle($delay_while_idle)
-    {
-        $this->delay_while_idle = $delay_while_idle;
-    }
-
 }
--- Entity/Notification.php
+++ Entity/Notification.php
@@ -8,12 +8,21 @@ class Notification
     private $body;
     private $title;
     private $icon;
+    private $sound;
+    private $badge;
+    private $tag;
+    private $color;
+    private $click_action;
+    private $body_loc_key;
+    private $body_loc_args;
+    private $title_loc_key;
+    private $title_loc_args;
 
-    public function __construct($title, $body, $icon = "Default")
+    public function __construct($title, $body = null)
     {
         $this->title = $title;
         $this->body = $body;
-        $this->icon = $icon;
+        $this->icon = "Default";
     }
 
     /**
@@ -25,6 +34,17 @@ class Notification
     }
 
     /**
+     * @param mixed $body
+     * @return Notification
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
      * @return mixed
      */
     public function getTitle()
@@ -33,11 +53,203 @@ class Notification
     }
 
     /**
-     * @return mixed
+     * @param mixed $title
+     * @return Notification
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return string
      */
     public function getIcon()
     {
         return $this->icon;
     }
+
+    /**
+     * @param string $icon
+     * @return Notification
+     */
+    public function setIcon($icon)
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isSound()
+    {
+        return $this->sound;
+    }
+
+
+    public function enableSound(){
+
+        $this->sound = true;
+
+        return $this;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getBadge()
+    {
+        return $this->badge;
+    }
+
+    /**
+     * @param mixed $badge
+     * @return Notification
+     */
+    public function setBadge($badge)
+    {
+        $this->badge = $badge;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTag()
+    {
+        return $this->tag;
+    }
+
+    /**
+     * @param mixed $tag
+     * @return Notification
+     */
+    public function setTag($tag)
+    {
+        $this->tag = $tag;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param string $color
+     * @return Notification
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClick_action()
+    {
+        return $this->click_action;
+    }
+
+    /**
+     * @param mixed $click_action
+     * @return Notification
+     */
+    public function setClick_action($click_action)
+    {
+        $this->click_action = $click_action;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody_loc_key()
+    {
+        return $this->body_loc_key;
+    }
+
+    /**
+     * @param mixed $body_loc_key
+     * @return Notification
+     */
+    public function setBody_loc_key($body_loc_key)
+    {
+        $this->body_loc_key = $body_loc_key;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody_loc_args()
+    {
+        return $this->body_loc_args;
+    }
+
+    /**
+     * @param mixed $body_loc_args
+     * @return Notification
+     */
+    public function setBody_loc_args($body_loc_args)
+    {
+        $this->body_loc_args = $body_loc_args;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle_loc_key()
+    {
+        return $this->title_loc_key;
+    }
+
+    /**
+     * @param mixed $title_loc_key
+     * @return Notification
+     */
+    public function setTitle_loc_key($title_loc_key)
+    {
+        $this->title_loc_key = $title_loc_key;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle_loc_args()
+    {
+        return $this->title_loc_args;
+    }
+
+    /**
+     * @param mixed $title_loc_args
+     * @return Notification
+     */
+    public function setTitle_loc_args($title_loc_args)
+    {
+        $this->title_loc_args = $title_loc_args;
+
+        return $this;
+    }
+
 
 }
